768f65e63514509d7041fc1b7f7ca22c1f92ccf6
sarveshlv <63788526+sarveshlv@users.noreply.github.com>
2021-08-08 01:54:53 +0530

    entry details page added

--- app/src/main/java/com/example/upass/app_home.java
+++ app/src/main/java/com/example/upass/app_home.java
@@ -2,10 +2,12 @@ package com.example.upass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class app_home extends AppCompatActivity {
 
@@ -18,16 +20,24 @@ public class app_home extends AppCompatActivity {
 
         b1 = findViewById(R.id.acc_btn);
         b2 = findViewById(R.id.button2);
+        ImageButton b3 = findViewById(R.id.imageButton);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b1.setBackgroundColor(Color.rgb(42,55,97));
+
             }
         });
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b2.setBackgroundColor(Color.rgb(42,55,97));
+
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(app_home.this, enter_details.class);
+                startActivity(intent);
             }
         });
     }
