031cbee1b08029a59114edf53ae3ebebb49c5ba9
ShridharaHegde <63788345+ShridharaHegde@users.noreply.github.com>
2021-08-07 21:48:53 +0530

    Revert "On click listener updated"
    This reverts commit ff6ded6977bcd5be83fefb82b4fd077c95d1d348.

--- app/src/main/java/com/example/upass/app_home.java
+++ app/src/main/java/com/example/upass/app_home.java
@@ -2,33 +2,13 @@ package com.example.upass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class app_home extends AppCompatActivity {
-
-    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_home);
-
-        b1 = findViewById(R.id.acc_btn);
-        b2 = findViewById(R.id.button2);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                b1.setBackgroundColor(Color.rgb(42,55,97));
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                b2.setBackgroundColor(Color.rgb(42,55,97));
-            }
-        });
     }
 }
--- app/src/main/java/com/example/upass/create_account.java
+++ app/src/main/java/com/example/upass/create_account.java
@@ -2,31 +2,13 @@ package com.example.upass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 public class create_account extends AppCompatActivity {
-
-    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-
-        b1 = findViewById(R.id.continue_btn);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(create_account.this, app_home.class);
-                startActivity(intent);
-
-            }
-        });
     }
 }
